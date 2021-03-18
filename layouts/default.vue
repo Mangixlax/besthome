ec6674d513f1d4b0a58feebd961c98adba348ca9
Sergey Mihalickiy <rcds09@yandex.ru>
2021-03-18 23:56:54 +0300

    Added: top line with store; header with navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,26 @@
-<template>
-  <div>
-    <Nuxt />
-  </div>
+<template lang="pug">
+  div
+    top-line(v-if="!topLineIsHidden" @close="onToggleHiddenMode")
+    base-header
+    nuxt
+    div(style="height: 2000px")
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script lang="ts">
+import Vue from 'vue'
+import { Component, namespace } from 'nuxt-property-decorator'
+import TopLine from '~/components/TopLine/TopLine.vue'
+import TypoText from '~/components/Base/TypoText.vue'
+import { SettingsTopLineState } from '~/store/SettingsTopLine'
+import BaseHeader from '~/components/BaseHeader/BaseHeader.vue'
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
+const SettingsTopLineStore = namespace('SettingsTopLine')
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+@Component({
+  components: { BaseHeader, TypoText, TopLine },
+})
+export default class DefaultLayout extends Vue {
+  @SettingsTopLineStore.Getter('isHidden') topLineIsHidden!: SettingsTopLineState['hidden']
+  @SettingsTopLineStore.Action('toggleHiddenMode') onToggleHiddenMode!: () => void
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+</script>
