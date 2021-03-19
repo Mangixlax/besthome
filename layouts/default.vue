a6a9b9782e5c8a83017184e0d552429e87f62281
Sergey Mihalickiy <rcds09@yandex.ru>
2021-03-20 01:13:43 +0300

    Added gsap, jquery; Created cursor mover

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,7 @@
     nuxt
     div(style="height: 2000px")
     base-header-mobile
+    cursor-mover
 </template>
 
 <script lang="ts">
@@ -15,11 +16,12 @@ import TypoText from '~/components/Base/TypoText.vue'
 import { SettingsTopLineState } from '~/store/SettingsTopLine'
 import BaseHeader from '~/components/BaseHeader/BaseHeader.vue'
 import BaseHeaderMobile from '~/components/BaseHeaderMobile/BaseHeaderMobile.vue'
+import CursorMover from '~/components/CursorMover.vue'
 
 const SettingsTopLineStore = namespace('SettingsTopLine')
 
 @Component({
-  components: { BaseHeaderMobile, BaseHeader, TypoText, TopLine },
+  components: { CursorMover, BaseHeaderMobile, BaseHeader, TypoText, TopLine },
 })
 export default class DefaultLayout extends Vue {
   @SettingsTopLineStore.Getter('isHidden') topLineIsHidden!: SettingsTopLineState['hidden']
