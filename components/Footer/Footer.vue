df3bedeb28b12e407e8ef63368ea9efbc93d3300
Aslan Dzhangetov <>
2021-04-23 23:39:15 +0300

    [PF-932] Added footer component

---
+++ components/Footer/Footer.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  footer(:class="$style['footer']")
+    footer-fast-links
+    footer-company-description
+    base-bread-crumbs
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import BaseBreadCrumbs from '~/components/Base/BaseBreadCrumbs.vue'
+import FooterFastLinks from '~/components/Footer/FooterFastLinks.vue' 
+import FooterCompanyDescription from '~/components/Footer/FooterCompanyDescription.vue' 
+
+@Component({
+  components: { BaseBreadCrumbs, FooterFastLinks, FooterCompanyDescription }
+})
+export default class Footer extends Vue {} 
+</script>
+
+<style lang="sass" module>
+.footer
+  width: 100%
+  magin: 0 auto
+  background: rgba(17, 17, 17, 0.08)
+</style>
