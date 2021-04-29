c6f48653137c6a9971963fac85c175255dfb4d03
Aslan Dzhangetov <>
2021-04-29 11:19:30 +0300

    [PF-934] Created Base Accordeons component and added CommonAccordeonItem javascript component

---
+++ components/Common/CommonAccordeonItem.vue
@@ -0,0 +1,119 @@
+<template lang="pug">
+  section(:class="$style['accordion']" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question")
+    header(
+      :class="$style['accordion__header']"
+      @click="onClick"
+    )
+      typo-text(tag="h3" version="style-4" :class="$style['accordion__header-title']" itemprop="name")
+        | {{ title }}
+      svg-icon(
+        :name="show ? 'accordion-minus' : 'accordion-plus'"
+        :class="$style['accordion__header-button']"
+      )
+    div(:style="{ height: myHeight }" :class="$style['accordion__body']" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer")
+      div(ref="textContainer" itemprop="text")
+        slot
+</template>
+
+<script>
+import TypoText from '~/components/Base/TypoText.vue'
+
+export default {
+  name: 'AccordionItem',
+  components: { TypoText },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      show: this.active,
+      myHeight: '0px',
+    }
+  },
+  methods: {
+    onClick() {
+      if (this.show) {
+        this.hideAccordion()
+      } else {
+        this.showAccordion()
+      }
+
+      if (this.$parent.$children.length) {
+        for (const $child of this.$parent.$children) {
+          if ($child._name.includes('AccordionItem') && this._uid !== $child._uid) {
+            $child.hideAccordion()
+          }
+        }
+      }
+    },
+    hideAccordion() {
+      this.myHeight = '0px'
+      this.show = false
+    },
+    showAccordion() {
+      this.myHeight = this.$refs.textContainer.clientHeight + 'px'
+      this.show = true
+    },
+  },
+  mounted() {
+    if (this.active) {
+      this.showAccordion()
+    }
+  },
+}
+</script>
+
+<style lang="sass" module>
+.accordion
+  box-sizing: border-box
+  border-bottom: solid 1px $color-white-4
+
+  &__header
+    position: relative
+    display: flex
+    align-items: center
+    cursor: pointer
+    padding: 24px 0
+    justify-content: space-between
+
+    &-title
+      color: $color-white-96
+      max-width: 85%
+
+    &-button
+      width: 30px
+      height: 30px
+
+  &__body
+    display: flex
+    transition: all 1s ease
+    overflow: hidden
+    flex-direction: column-reverse
+
+    > div
+      padding: 24px 32px
+
+    +mobile
+      padding-left: 16px
+      padding-right: 16px
+
+      > div
+        padding: 24px 0
+
+  &__header h3
+    margin: 0
+
+  &__body p:first-child
+    margin-top: 0
+
+.reverse-transition
+  display: flex
+  flex-direction: column-reverse
+</style>
