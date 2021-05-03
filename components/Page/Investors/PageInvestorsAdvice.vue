c15265e32d384d5c628aaecfbed60b2f37e7304b
Aslan Dzhangetov <>
2021-05-03 21:23:36 +0300

    Created Investors page and components

---
+++ components/Page/Investors/PageInvestorsAdvice.vue
@@ -0,0 +1,123 @@
+<template lang="pug">
+  section(:class="$style['advice']")
+    div(:class="$style['advice__container']")
+      typo-text(
+        v-for="(paragraph, i) in investorsPageAdvice.intoduction"
+        :key="i"
+        tag="p"
+        version="style-6"
+        :class="$style['advice__container-text']"
+      ) {{ paragraph }}
+      typo-text(
+        tag="h2"
+        version="style-2"
+        :class="$style['advice__container-title']"
+      ) {{ investorsPageAdvice.title }}
+      div(:class="$style['cards']")
+        section(
+          v-for="(card, i) in investorsPageAdvice.cards"
+          :key="i"
+          :class="[$style['card'], $style[`card-${i}`]]"
+        )
+          typo-text(
+            tag="h2"
+            version="style-4"
+            :class="$style['card-title']"
+          ) {{ card.title }}
+          typo-text(
+            tag="p"
+            version="style-6"
+            :class="$style['card-text']"
+          ) {{ card.text }}
+</template>
+
+<script lang="ts">
+import TypoText from '~/components/Base/TypoText.vue'
+
+export default {
+  name: "PageInvestorsTitle",
+  components: { TypoText, },
+  data() {
+    return {
+      investorsPageAdvice: {
+        intoduction: [
+          'Alanya is one of the most promising regions for profitable investment of finances. Mahmutlar, Kargicak, Kestel, Oba, Cikcilli-Alanya and its suburbs have been actively built up over the past years. The infrastructure of residential complexes is not inferior to five-star hotels. And the high quality of the interior decoration of apartments and villas is impressive.',
+          'Since 2005, BestHome Construction has been successfully cooperating with investors in Russia, Ukraine, Kazakhstan, America, Denmark, Norway, Germany and other countries, helping them to increase their capital and invest profitably by buying houses and apartments in Turkey. The success of our work lies in the observance of the basic principles of the company.'
+        ],
+        title: "Investment advice from BestHome Construction is:",
+        cards: [
+          {
+            title: "1.",
+            text: "Individual approach to each client-we will select from 5 options for buying real estate, ready-made business in the field of agriculture or service sector.",
+          },
+          {
+            title: "2.",
+            text: "Assistance in obtaining Turkish citizenship – we will answer all your questions in detail.",
+          },
+          {
+            title: "3.",
+            text: "Assistance in obtaining Turkish citizenship – we will answer all your questions in detail.",
+          },
+          {
+            title: "4.",
+            text: "Assistance in obtaining Turkish citizenship – we will answer all your questions in detail.",
+          },
+        ],
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass" module>
+.advice
+  width: 100%
+
+  &__container
+    display: flex
+    flex-direction: column
+    max-width: 1040px
+    padding: 80px 24px
+    margin: 0 auto
+
+    &-title
+      margin: 0
+      margin-bottom: 40px
+      max-width: 700px
+    
+    &-text
+      margin:0
+      margin-bottom: 16px
+
+.cards
+  display: flex
+
+.card
+  padding: 32px
+  height: 474px
+  width: 248px
+  
+  &-title
+    color: $color-blue-100
+    margin: 0
+    margin-bottom: 33px
+    position: relative
+
+  &-text
+    margin: 0
+
+  &.card-0
+    border: 1px solid  $color-blue-16
+
+  &.card-1
+    background: $color-blue-4
+
+  &.card-2
+    background: $color-blue-8
+
+  &.card-3
+    background: $color-blue-16
+  
+  &.card-4
+    background: $color-blue-24
+</style>
---
+++ pages/investors/index.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+  main
+    page-investors-title
+    base-post
+      typo-text(
+        tag="h3"
+        version="style-3"
+      ) Bet on long-term cooperation
+      typo-text(
+      ) We know that the success of cooperation with us primarily depends on the stability of our clients ' income. We rely on a long-term mutually beneficial partnership. The more our investors earn, the more promising our business is.
+      typo-text(
+        tag="p"
+        version="style-5"
+      )
+        | To date, the company has
+        typo-text(
+          tag="a"
+          href="#"
+          version="style-5"
+        ) 30 completed projects
+        | .
+    page-investors-advice
+    base-post(flip text-flip)
+      typo-text(
+        tag="h3"
+        version="style-3"
+      ) Responsible approach to work
+      typo-text(
+        tag="p"
+        version="style-5"
+      ) We have strict requirements for each of our employees. We constantly monitor new trends in the real estate market. Our employees regularly replenish their knowledge reserves in order to successfully implement them in their clients ' business projects.
+      typo-text(
+        tag="p"
+        version="style-5"
+      )
+        | More
+        typo-text(
+          tag="a"
+          href="#"
+          version="style-5"
+        ) about Cleopatra Select 
+        | .
+    base-post
+      typo-text(
+        tag="h3"
+        version="style-3"
+      ) Bet on long-term cooperation
+      typo-text(
+        tag="p"
+        version="style-5"
+      ) We know that the success of cooperation with us primarily depends on the stability of our clients ' income. We rely on a long-term mutually beneficial partnership. The more our investors earn, the more promising our business is.
+      typo-text(
+        tag="p"
+        version="style-5"
+      )
+        | To date, the company has
+        typo-text(
+          tag="a"
+          href="#"
+          version="style-5"
+        ) 30 completed projects
+        | .
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import PageInvestorsTitle from '~/components/Page/Investors/PageInvestorsTitle.vue'
+import PageInvestorsAdvice from '~/components/Page/Investors/PageInvestorsAdvice.vue'
+import BasePost from '~/components/Base/BasePost.vue'
+import TypoText from '~/components/Base/TypoText.vue'
+
+@Component({
+  components: { PageInvestorsTitle, PageInvestorsAdvice, BasePost, TypoText },
+})
+export default class IndexPage extends Vue {}
+</script>
+
+<style lang="sass" module></style>
