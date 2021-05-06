b9fbd113999af1c8e0fe6a52ae4f116640586536
Aslan Dzhangetov <>
2021-05-06 18:14:07 +0300

    Created PageCompanyOurTeam components and suppurted files

---
+++ pages/company/our-team/index.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  main
+    page-company-our-team
+</template>
+
+<script lang="ts">
+import PageCompanyOurTeam from '~/components/Page/Company/PageCompanyOurTeam.vue'
+
+export default {
+  name: 'our-team',
+  components: { PageCompanyOurTeam },
+}
+</script>
+
+<style lang="sass" module></style>
--- store/index.ts
+++ store/index.ts
@@ -6,6 +6,96 @@ import { GetterTree, ActionTree, MutationTree, ActionContext } from 'vuex'
 export const state = () => ({
   isTouchDevice: false as boolean,
   logoSubTitle: 'Construction' as string,
+  ourCompanyCardInfo: [
+    {
+      id: 1,
+      image: 'person-1.png',
+      name: 'Tim Cook',
+      position: 'Chief Executive Officer',
+      info: [
+        'Greg “Joz” Joswiak is Apple’s senior vice president of Worldwide Marketing, reporting to CEO Tim Cook.',
+        'Since joining Apple in June 1986, Joz has played a pivotal role in developing and launching some of the world’s most beloved consumer products, including the original iPod and iPhone. Joz began his career at Apple working on the early Macintosh computers and supporting the Mac’s community of third-party developers.',
+        'Joz has over 30 years of marketing and management experience at Apple, most recently as the vice president of Apple Product Marketing where he managed the product marketing and product management teams responsible for Apple’s entire product lineup. Joz graduated with a Bachelor of Science in Computer Engineering from the University of Michigan in 1986.'
+      ],
+    },
+    {
+      id: 2,
+      image: 'person-2.png',
+      name: 'Greg “Joz” Joswiak',
+      position: 'Senior Vice President, Worldwide Marketing',
+      info: [
+        'Greg “Joz” Joswiak is Apple’s senior vice president of Worldwide Marketing, reporting to CEO Tim Cook.',
+        'Since joining Apple in June 1986, Joz has played a pivotal role in developing and launching some of the world’s most beloved consumer products, including the original iPod and iPhone. Joz began his career at Apple working on the early Macintosh computers and supporting the Mac’s community of third-party developers.',
+        'Joz has over 30 years of marketing and management experience at Apple, most recently as the vice president of Apple Product Marketing where he managed the product marketing and product management teams responsible for Apple’s entire product lineup. Joz graduated with a Bachelor of Science in Computer Engineering from the University of Michigan in 1986.'
+      ],
+    },
+    {
+      id: 3,
+      image: 'person-3.png',
+      name: 'Tim Cook',
+      position: 'Chief Executive Officer',
+      info: [
+        'Greg “Joz” Joswiak is Apple’s senior vice president of Worldwide Marketing, reporting to CEO Tim Cook.',
+        'Since joining Apple in June 1986, Joz has played a pivotal role in developing and launching some of the world’s most beloved consumer products, including the original iPod and iPhone. Joz began his career at Apple working on the early Macintosh computers and supporting the Mac’s community of third-party developers.',
+        'Joz has over 30 years of marketing and management experience at Apple, most recently as the vice president of Apple Product Marketing where he managed the product marketing and product management teams responsible for Apple’s entire product lineup. Joz graduated with a Bachelor of Science in Computer Engineering from the University of Michigan in 1986.'
+      ],
+    },
+    {
+      id: 4,
+      image: 'person-4.png',
+      name: 'Tim Cook',
+      position: 'Chief Executive Officer',
+      info: [
+        'Greg “Joz” Joswiak is Apple’s senior vice president of Worldwide Marketing, reporting to CEO Tim Cook.',
+        'Since joining Apple in June 1986, Joz has played a pivotal role in developing and launching some of the world’s most beloved consumer products, including the original iPod and iPhone. Joz began his career at Apple working on the early Macintosh computers and supporting the Mac’s community of third-party developers.',
+        'Joz has over 30 years of marketing and management experience at Apple, most recently as the vice president of Apple Product Marketing where he managed the product marketing and product management teams responsible for Apple’s entire product lineup. Joz graduated with a Bachelor of Science in Computer Engineering from the University of Michigan in 1986.'
+      ],
+    },
+    {
+      id: 5,
+      image: 'person-5.png',
+      name: 'Tim Cook',
+      position: 'Chief Executive Officer',
+      info: [
+        'Greg “Joz” Joswiak is Apple’s senior vice president of Worldwide Marketing, reporting to CEO Tim Cook.',
+        'Since joining Apple in June 1986, Joz has played a pivotal role in developing and launching some of the world’s most beloved consumer products, including the original iPod and iPhone. Joz began his career at Apple working on the early Macintosh computers and supporting the Mac’s community of third-party developers.',
+        'Joz has over 30 years of marketing and management experience at Apple, most recently as the vice president of Apple Product Marketing where he managed the product marketing and product management teams responsible for Apple’s entire product lineup. Joz graduated with a Bachelor of Science in Computer Engineering from the University of Michigan in 1986.'
+      ],
+    },
+    {
+      id: 6,
+      image: 'person-6.png',
+      name: 'Tim Cook',
+      position: 'Chief Executive Officer',
+      info: [
+        'Greg “Joz” Joswiak is Apple’s senior vice president of Worldwide Marketing, reporting to CEO Tim Cook.',
+        'Since joining Apple in June 1986, Joz has played a pivotal role in developing and launching some of the world’s most beloved consumer products, including the original iPod and iPhone. Joz began his career at Apple working on the early Macintosh computers and supporting the Mac’s community of third-party developers.',
+        'Joz has over 30 years of marketing and management experience at Apple, most recently as the vice president of Apple Product Marketing where he managed the product marketing and product management teams responsible for Apple’s entire product lineup. Joz graduated with a Bachelor of Science in Computer Engineering from the University of Michigan in 1986.'
+      ],
+    },
+    {
+      id: 7,
+      image: 'person-7.png',
+      name: 'Tim Cook',
+      position: 'Chief Executive Officer',
+      info: [
+        'Greg “Joz” Joswiak is Apple’s senior vice president of Worldwide Marketing, reporting to CEO Tim Cook.',
+        'Since joining Apple in June 1986, Joz has played a pivotal role in developing and launching some of the world’s most beloved consumer products, including the original iPod and iPhone. Joz began his career at Apple working on the early Macintosh computers and supporting the Mac’s community of third-party developers.',
+        'Joz has over 30 years of marketing and management experience at Apple, most recently as the vice president of Apple Product Marketing where he managed the product marketing and product management teams responsible for Apple’s entire product lineup. Joz graduated with a Bachelor of Science in Computer Engineering from the University of Michigan in 1986.'
+      ],
+    },
+    {
+      id: 8,
+      image: 'person-8.png',
+      name: 'Tim Cook',
+      position: 'Chief Executive Officer',
+      info: [
+        'Greg “Joz” Joswiak is Apple’s senior vice president of Worldwide Marketing, reporting to CEO Tim Cook.',
+        'Since joining Apple in June 1986, Joz has played a pivotal role in developing and launching some of the world’s most beloved consumer products, including the original iPod and iPhone. Joz began his career at Apple working on the early Macintosh computers and supporting the Mac’s community of third-party developers.',
+        'Joz has over 30 years of marketing and management experience at Apple, most recently as the vice president of Apple Product Marketing where he managed the product marketing and product management teams responsible for Apple’s entire product lineup. Joz graduated with a Bachelor of Science in Computer Engineering from the University of Michigan in 1986.'
+      ],
+    },
+  ],
 })
 
 export type RootState = ReturnType<typeof state>
@@ -21,6 +111,9 @@ interface RootActionContext extends ActionContext<RootState, RootState> {}
 export const getters: GetterTree<RootState, RootState> = {
   getLogoSubTitle(state: RootState) {
     return state.logoSubTitle
+  },
+  getPersonById: (state) => (id: any) => {
+    return state.ourCompanyCardInfo.find(person => person.id == id)
   },
 }
 
