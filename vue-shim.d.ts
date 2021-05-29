859afe88f4ec31b6a9d5b4a49b291427e26d5cb3
Sergey Mihalickiy <rcds09@yandex.ru>
2021-05-29 22:21:43 +0300

    Updated declarations for modules vue-shim.d.ts

--- vue-shim.d.ts
+++ vue-shim.d.ts
@@ -1,8 +1,32 @@
 import Vue from 'vue'
+import {
+  DefaultComputed,
+  DefaultData,
+  DefaultMethods,
+  DefaultProps,
+  PropsDefinition,
+} from 'vue/types/options'
 
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
+}
+
+declare module 'vue/types/options' {
+  interface ComponentOptions<
+    V extends Vue,
+    Data = DefaultData<V>,
+    Methods = DefaultMethods<V>,
+    Computed = DefaultComputed,
+    PropsDef = PropsDefinition<DefaultProps>,
+    Props = DefaultProps,
+  > {
+    functional?: boolean
+  }
+
+  interface RenderContext<Props = DefaultProps> {
+    $style: { [key: string]: string }
+  }
 }
 
 interface Document {
