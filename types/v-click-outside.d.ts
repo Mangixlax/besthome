4f5a9fac83003ea69bd6a5f9addc266564218bae
Sergey Mihalickiy <rcds09@yandex.ru>
2021-05-29 17:15:43 +0300

    Fixed types of v-click-outside

--- types/v-click-outside.d.ts
+++ types/v-click-outside.d.ts
@@ -1,1 +1,21 @@
-declare module 'v-click-outside' {}
+declare module 'v-click-outside' {
+  import { DirectiveBinding } from 'vue/types/options'
+  import { VNode } from 'vue'
+
+  export type DirectiveFunction = (
+    el: HTMLElement,
+    binding: DirectiveBinding,
+    vnode: VNode,
+    oldVnode: VNode,
+  ) => void
+
+  interface IDirectiveOptions {
+    bind?: DirectiveFunction
+    inserted?: DirectiveFunction
+    update?: DirectiveFunction
+    componentUpdated?: DirectiveFunction
+    unbind?: DirectiveFunction
+  }
+
+  export default IDirectiveOptions
+}
