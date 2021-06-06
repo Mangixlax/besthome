8e8de3cc2d031f72f57d82a057236331b16aef8e
Sergey Mihalickiy <rcds09@yandex.ru>
2021-06-06 17:27:25 +0300

    Add property $style to vue instances

--- vue-shim.d.ts
+++ vue-shim.d.ts
@@ -12,6 +12,12 @@ declare module '*.vue' {
   export default Vue
 }
 
+declare module 'vue/types/vue' {
+  interface Vue {
+    $style: { [key: string]: string }
+  }
+}
+
 declare module 'vue/types/options' {
   interface ComponentOptions<
     V extends Vue,
