63026b848f0d4abb37a6eb788121dcb5ecf8f5b8
Sergey Mihalickiy <rcds09@yandex.ru>
2021-06-06 18:25:50 +0300

    Refactoring infrastructure slider for render with data from api

--- components/Page/Projects/PageProjectsInfrastructureSlider.vue
+++ components/Page/Projects/PageProjectsInfrastructureSlider.vue
@@ -1,27 +1,18 @@
 <template lang="pug">
   div(:class="$style['container']")
     div(:class="$style['container__header']")
-      typo-text(
-        tag="h2"
-        version="style-4"
-      ) Infrastructure
+      typo-text(tag="h2" version="style-4") {{ data.name }}
     div(:class="$style['container__body']")
       swiper(ref="swiper" :class="$style['slider']" class="swiper" :options="swiperOption")
         swiper-slide(
-          v-for="(slide,i) in projectsInfrastructureSliderData"
+          v-for="(slide,i) in data.items"
           :key="i"
           :class="$style['slide']"
         )
           div(:class="$style['container__body-content']")
-            svg-icon(:name="`infrastructures/${slide.svg}`")
-            typo-text(
-              tag="h2"
-              version="style-5"
-            ) {{ slide.title }}
-            typo-text(
-              tag="h2"
-              version="style-7"
-            ) {{ slide.text }}
+            svg-icon(:name="`infrastructures/${slide.icon}`")
+            typo-text(tag="h3" version="style-5" v-html="slide.title")
+            typo-text(tag="p" version="style-7" v-html="slide.text")
         div(slot="pagination" :class="$style['navigation']")
           div(:class="['swiper-pagination-progressbar', $style['swiper-pagination-progressbar']]")
             div(class="status-bar")
@@ -30,130 +21,61 @@
               svg-icon(name="slider-prev-arrow-blue")
             button(:class="[$style['swiper-button-next']]" @click.prevent="$refs.swiper.swiperInstance.slideNext()")
               svg-icon(name="slider-next-arrow-blue")
-      div(:class="$style['footer']") 
-        typo-text(
-          tag="p"
-          version="style-5"
-          :class="$style['footer__link-text']"
-        ) Cleopatra Select 
-        typo-text(
-          tag="nuxt-link"
-          version="style-5"
-          :to="localePath({ name: 'index' })"
-          :class="$style['footer__link-text--underline']"
-        ) сhoose an apartment
-        | .
-        typo-text(
-          tag="nuxt-link"
-          version="none"
-          :to="localePath({ name: 'index' })"
-          :class="$style['footer__link-arrow']"
-        )
-          svg-icon(name="link-arrow-blue")
+      div(:class="$style['footer']" v-html="data.footer")
 </template>
 
-<script>
+<script lang="ts">
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
 import TypoText from '~/components/Base/TypoText.vue'
 import PageCompanyPersonalCard from '~/components/Page/Company/PageCompanyPersonalCard.vue'
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
 
-export default {
-  name: 'PageProjectsInfrastructureSlider',
+interface ISlider {
+  name: string
+  footer: string
+  items: ISliderItem[]
+}
+
+interface ISliderItem {
+  title: string
+  text: string
+  icon: string
+  sort_order: number
+}
+
+@Component({
   components: {
     Swiper,
     SwiperSlide,
     TypoText,
     PageCompanyPersonalCard,
   },
-  data() {
-    return {
-      swiperOption: {
-        breakpoints: {
-          // when window width is >= 900px
-          600: {
-            slidesPerView: 1,
-            spaceBetween: 24,
-          },
-          900: {
-            slidesPerView: 2,
-            spaceBetween: 32,
-          },
-          1200: {
-            slidesPerView: 3,
-            spaceBetween: 48,
-          },
-        },
+})
+export default class PageProjectsInfrastructureSlider extends Vue {
+  @Prop({ type: Object, default: () => {} }) data!: ISlider
+
+  public swiperOption: object = {
+    breakpoints: {
+      // when window width is >= 900px
+      600: {
+        slidesPerView: 1,
         spaceBetween: 24,
-        pagination: {
-          el: '.swiper-pagination-progressbar',
-          type: 'progressbar',
-        },
-        enabled: false,
       },
-      projectsInfrastructureSliderData: [
-        {
-          svg: 'car',
-          title: 'Underground parking',
-          text:
-            'Each building will have a two-floor underground parking garage, making it easy to access or exit the building. With parked vehicles removed, the neighbourhood will have a functional outdoor space for socializing.',
-        },
-        {
-          svg: 'floor-plans',
-          title: 'Intelligent floor plans',
-          text:
-            'Ground floor apartments will have loggias and several floor plan options to choose from. Units on top floors will have higher ceilings and terraces, and will be perfect for more demanding clients.',
-        },
-        {
-          svg: 'bicycle',
-          title: 'Spacious bicycle rooms',
-          text:
-            'Spacious bicycle rooms on the ground floor will provide ample space for bicycle storage. Our aim is to encourage sustainable mobility in the city and provide a safe and convenient space to store your bikes.',
-        },
-        {
-          svg: 'car',
-          title: 'Underground parking',
-          text:
-            'Each building will have a two-floor underground parking garage, making it easy to access or exit the building. With parked vehicles removed, the neighbourhood will have a functional outdoor space for socializing.',
-        },
-        {
-          svg: 'floor-plans',
-          title: 'Intelligent floor plans',
-          text:
-            'Ground floor apartments will have loggias and several floor plan options to choose from. Units on top floors will have higher ceilings and terraces, and will be perfect for more demanding clients.',
-        },
-        {
-          svg: 'bicycle',
-          title: 'Spacious bicycle rooms',
-          text:
-            'Spacious bicycle rooms on the ground floor will provide ample space for bicycle storage. Our aim is to encourage sustainable mobility in the city and provide a safe and convenient space to store your bikes.',
-        },
-        {
-          svg: 'car',
-          title: 'Underground parking',
-          text:
-            'Each building will have a two-floor underground parking garage, making it easy to access or exit the building. With parked vehicles removed, the neighbourhood will have a functional outdoor space for socializing.',
-        },
-        {
-          svg: 'floor-plans',
-          title: 'Intelligent floor plans',
-          text:
-            'Ground floor apartments will have loggias and several floor plan options to choose from. Units on top floors will have higher ceilings and terraces, and will be perfect for more demanding clients.',
-        },
-        {
-          svg: 'bicycle',
-          title: 'Spacious bicycle rooms',
-          text:
-            'Spacious bicycle rooms on the ground floor will provide ample space for bicycle storage. Our aim is to encourage sustainable mobility in the city and provide a safe and convenient space to store your bikes.',
-        },
-      ],
-    }
-  },
-  computed: {
-    getOurCompanyCardInfo() {
-      return this.$store.state.ourCompanyCardInfo
+      900: {
+        slidesPerView: 2,
+        spaceBetween: 32,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 48,
+      },
     },
-  },
-  mounted() {},
+    spaceBetween: 24,
+    pagination: {
+      el: '.swiper-pagination-progressbar',
+      type: 'progressbar',
+    },
+  }
 }
 </script>
 
@@ -167,15 +89,15 @@ export default {
     padding: 40px 24px
 
   &__header
-    max-width: 912px
+    max-width: 864px
     margin: 0 auto
 
     h2
       margin: 0
 
   &__body
-    padding: 48px 24px 0 24px
-    max-width: 911px
+    padding: 48px 24px 0
+    max-width: 864px + 48px
     margin: 0 auto
 
     @media (max-width: 900px)
@@ -183,9 +105,14 @@ export default {
 
     &-content
       max-width: 256px
-      display: flex
-      flex-direction: column
-      align-items: flex-start
+
+      h3
+        color: $color-black-100
+        margin: 16px 0
+
+      p
+        letter-spacing: 0.003em
+        color: $color-black-80
 
       @media (max-width: 600px)
         max-width: 100%
@@ -250,30 +177,16 @@ export default {
   margin: 0 auto
   flex-wrap: wrap
 
-  &__link
-    display: flex
-    align-items: center
-    flex-wrap: wrap
+  p
+    +style-5
 
-    &-text
-      margin: 0
-      white-space: nowrap
+  a
+    white-space: nowrap
+    text-decoration: underline
+    text-decoration-color: $color-blue-16
+    text-underline-offset: 7px
+    color: $color-blue-100
 
-      &--underline
-        white-space: nowrap
-        text-decoration: underline
-        text-decoration-color: $color-blue-16
-        text-underline-offset: 7px
-        color: $color-blue-100
-        margin-left: 0.5em
-
-        @media (max-width: 355px)
-          margin-left: initial
-
-    &-arrow
-      height: 26px
-
-      svg
-        height: 26px
-        width: 26px
+    @media (max-width: 355px)
+      margin-left: initial
 </style>
